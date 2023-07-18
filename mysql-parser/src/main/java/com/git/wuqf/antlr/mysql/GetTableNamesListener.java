564e446452a9408ab5fccc5edbec25eedb9e194d
GW00294495 <wuqingfeng234@163.com>
2023-07-18 10:12:22 +0800

    delete redundancy code

--- mysql-parser/src/main/java/com/git/wuqf/antlr/mysql/GetTableNamesListener.java
+++ mysql-parser/src/main/java/com/git/wuqf/antlr/mysql/GetTableNamesListener.java
@@ -10,20 +10,6 @@ import java.util.Set;
 
 public class GetTableNamesListener extends MySqlParserBaseListener {
 
-    public static void main(String[] args) {
-        String sql = "SELECT t1.column1,t1.column2,t1.column3,t2.xy from tableC t1 left join tableA t2 on t1.id=t2.oid where t1.column1 = 1 and t2.yy=6";
-        System.out.println(sql);
-        MySqlLexer lexer = new MySqlLexer(CharStreams.fromString(sql.toUpperCase()));
-        MySqlParser parser = new MySqlParser(new CommonTokenStream(lexer));
-        //定义GetTableNamesListener
-        GetTableNamesListener listener = new GetTableNamesListener();
-        ParseTreeWalker.DEFAULT.walk(listener, parser.sqlStatements());
-        Set<String> tableNameSet = listener.getTableNameSet();
-        for (String tableName : tableNameSet) {
-            System.out.println(tableName);
-        }
-    }
-
     private final Set<String> tableNameSet = new HashSet<>();
 
     @Override
